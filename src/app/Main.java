033fe02bee4a90bd09d041296f08c7bdeab68c24
Tiago Farinon <farinon@gmail.com>
2022-01-12 03:16:46 -0300

    Commit inicial - Criação do menu

---
+++ src/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		showMenu();
+	}
+	
+	public static void showMenu() {
+		var menu = 	"*****************************\n"
+				+ 	"*  Design Patterns - GoF    *\n"
+				+ 	"*  Tiago da Costa Farinon   *\n"
+				+ 	"*  Exemplos de funcionamento*\n"
+				+ 	"*****************************\n"
+				+ 	"                             \n"
+				+ 	"1 - Facade                   \n"
+				+   "2 - Tal tal                  \n"
+				+ 	"-----------------------------\n"
+				+ 	"Escolha o padrão que quer testar:";
+		System.out.println(menu);
+	}
+
+}
