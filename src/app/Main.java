bd4d19a7cfc0b39ab4c720818f4de20db62b35db
Tiago Farinon <farinon@gmail.com>
2022-01-12 03:34:14 -0300

    menu inicial pronto

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,15 +8,19 @@ public class Main {
 	}
 	
 	public static void showMenu() {
-		var menu = 	"*****************************\n"
-				+ 	"*  Design Patterns - GoF    *\n"
-				+ 	"*  Tiago da Costa Farinon   *\n"
-				+ 	"*  Exemplos de funcionamento*\n"
-				+ 	"*****************************\n"
-				+ 	"                             \n"
-				+ 	"1 - Facade                   \n"
-				+   "2 - Tal tal                  \n"
-				+ 	"-----------------------------\n"
+		var menu = 	"***************************************************\n"
+				+ 	"*  Design Patterns - GoF                          *\n"
+				+ 	"*  Tiago da Costa Farinon                         *\n"
+				+ 	"*  Exemplos de funcionamento                      *\n"
+				+ 	"***************************************************\n"
+				+ 	"* 1 - Abstract Factory         * 8  - Mediator    *\n"
+				+   "* 2 - Bridge                   * 9  - Memento     *\n"
+				+   "* 3 - Buider                   * 10 - Prototype   *\n"
+				+   "* 4 - Chain of Responsibility  * 11 - Proxy       *\n"
+			 	+   "* 5 - Composite                * 12 - State       *\n"
+				+   "* 6 - Facade                   * 13 - Visitor     *\n"
+				+   "* 7 - Flyweight                *                  *\n"
+				+ 	"***************************************************\n"
 				+ 	"Escolha o padrão que quer testar:";
 		System.out.println(menu);
 	}
