b48de995e61ff6ed9b427e0deb283ee64a9c4da8
Tiago Farinon <farinon@gmail.com>
2022-01-13 00:33:34 -0300

    composite refatoração inicial

--- src/designpatterns/composite/CompositeTester.java
+++ src/designpatterns/composite/CompositeTester.java
@@ -1,6 +1,9 @@
 package designpatterns.composite;
 
 import app.TesterInterface;
+
+import java.util.ArrayList;
+
 import app.AbstractTester;
 
 public class CompositeTester  extends AbstractTester implements TesterInterface{
@@ -47,7 +50,31 @@ public class CompositeTester  extends AbstractTester implements TesterInterface{
 	   }
 	@Override
 	public void start() {
-		showTitle(title);	
+		showTitle(title);
+Caixa caixaMaster = new Caixa(new ArrayList<Item>()); 
+		
+		caixaMaster.addItem(new Produto("Clean Code: A Handbook of Agile Software Craftsmanship", 202.71));
+		
+		Caixa caixaSmartphones = new Caixa(new ArrayList<Item>());
+		caixaSmartphones.addItem(new Produto("Samsung Galaxy S21 Ultra 5G", 6191.10));
+		caixaSmartphones.addItem(new Produto("Samsung Galaxy S20 FE", 2189));
+		caixaSmartphones.addItem(new Produto("iPhone 13", 7599));
+		caixaSmartphones.addItem(new Produto("iPhone 13 mini", 6599));
+		caixaMaster.addItem(caixaSmartphones);
+		
+		Caixa caixaNotebooks = new Caixa(new ArrayList<Item>());
+		caixaNotebooks.addItem(new Produto("Acer Nitro AN515-44-R5YZ", 5499));
+		caixaNotebooks.addItem(new Produto("Dell G15", 5301));
+		caixaMaster.addItem(caixaNotebooks);
+		
+		Caixa caixaFerramentas = new Caixa(new ArrayList<Item>());
+		caixaFerramentas.addItem(new Produto("Martelo de Unha 25mm", 37.30));
+		caixaFerramentas.addItem(new Produto("Trena 5m", 17.10));
+		caixaFerramentas.addItem(new Produto("Alicate 7", 25));
+		caixaFerramentas.addItem(new Produto("Chave de Fenda Ponta Cruzada 1/8x6 Polegadas", 5.90));
+		caixaMaster.addItem(caixaFerramentas);
+		
+		System.out.println("VALOR TOTAL = " + caixaMaster.calcularPrecoFinal());
 	}
 
 }
