635453caa4f4b805256ff44f973a6814b3763976
Tiago Farinon <farinon@gmail.com>
2022-01-13 16:34:40 -0300

    chainofresponsibility - padronização de classe

---
+++ src/designpatterns/chainofresponsibility/Main.java
@@ -0,0 +1,20 @@
+package designpatterns.chainofresponsibility;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//Instancio os Handlers
+		LogInfoHandler logInfoHandler = new LogInfoHandler();
+		LogWarningHandler logWarningHandler = new LogWarningHandler();
+		LogErrorHandler logErrorHandler = new LogErrorHandler();
+		
+		logInfoHandler.next(logWarningHandler);
+		logWarningHandler.next(logErrorHandler);
+		
+		
+		String response = logInfoHandler.handle("error");
+		System.out.println(response);
+	}
+
+}
