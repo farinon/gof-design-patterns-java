3d36fb708647357381d71d43b1156e83a01b7725
Tiago Farinon <farinon@gmail.com>
2022-01-13 00:59:26 -0300

    memento - refatoração inicial

--- src/designpatterns/memento/MementoTester.java
+++ src/designpatterns/memento/MementoTester.java
@@ -47,7 +47,30 @@ public class MementoTester  extends AbstractTester implements TesterInterface{
 	   }
 	@Override
 	public void start() {
-		showTitle(title);	
+		showTitle(title);
+		Originator originador = new Originator();
+	    History armazenador = new History();
+	    
+	    
+	    originador.setState("State #1");
+	    
+	    
+	    originador.setState("State #2");
+	    armazenador.saveState(originador.generateMemento());
+	    
+	    
+	    originador.setState("State #3");
+	    armazenador.saveState(originador.generateMemento());
+	    
+	    
+	    originador.setState("State #4");
+	    System.out.println("Current State: " + originador.getState());		
+	    
+	    originador.setStateFromMemento(armazenador.getState(0));
+	    System.out.println("First saved State ----- " + originador.getState());
+	    
+	    originador.setStateFromMemento(armazenador.getState(1));
+	    System.out.println("Second saved State ----- " + originador.getState());
 	}
 
 }
